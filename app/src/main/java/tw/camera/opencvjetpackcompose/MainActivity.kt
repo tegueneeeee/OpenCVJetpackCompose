c77fe9755cc7d17e21d356053dc16cf9ea3ef138
Kim Taekwon <kimxordnjs@naver.com>
2022-03-06 22:47:29 +0900

    [ADD]Setting for Jetpack compose And CameraX

--- app/src/main/java/tw/camera/opencvjetpackcompose/MainActivity.kt
+++ app/src/main/java/tw/camera/opencvjetpackcompose/MainActivity.kt
@@ -1,29 +1,31 @@
 package tw.camera.opencvjetpackcompose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import tw.camera.opencvjetpackcompose.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import tw.camera.opencvjetpackcompose.ui.theme.OpenCVJetpackComposeTheme
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+        setContent {
+            OpenCVJetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    (Modifier.fillMaxSize())
+                }
+            }
+        }
     }
 
     /**
      * A native method that is implemented by the 'opencvjetpackcompose' native library,
      * which is packaged with this application.
      */
-    external fun stringFromJNI(): String
 
     companion object {
         // Used to load the 'opencvjetpackcompose' library on application startup.
